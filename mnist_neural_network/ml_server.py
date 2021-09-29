40e6cd61bc24cd03a689ce4302129e5e46088896
judith reker <judith.reker@gmail.com>
2021-09-29 11:51:15 +0200

    reorder package, create new model

--- mnist_neural_network/ml_server.py
+++ mnist_neural_network/ml_server.py
@@ -6,6 +6,17 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+# 'normal' model that outputs just the final layer
+model = tf.keras.models.load_model('mnist_model.h5') # ck path!
+
+model_all_layers = tf.keras.models.Model(
+    model.inputs,
+    [layer.output for layer in model.layers], # currently: 3 layers
+    )
+
+
+
+
 @app.route('/')
 def index():
     return 'Welcome to the model server!'
