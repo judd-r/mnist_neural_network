a0956bde6d4efc575f2c057f690e3c89160d53d1
judith reker <judith.reker@gmail.com>
2021-09-29 12:41:13 +0200

    create streamlit app

--- mnist_neural_network/ml_server.py
+++ mnist_neural_network/ml_server.py
@@ -1,7 +1,6 @@
 import json
 import tensorflow as tf
 import numpy as np
-import random
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -14,11 +13,30 @@ model_all_layers = tf.keras.models.Model(
     [layer.output for layer in model.layers], # currently: 3 layers
     )
 
+# load dataset again, this time only X_test
+_, (X_test, _) = tf.keras.datasets.mnist.load_data()
+X_test = X_test / 255. # not reshaping it here bec. want to visualise?
+
+def get_prediction():
+    """ Return outputs (incl. y = [0-9] ?) of predict method on NN model
+        showing output for all layers
+    """
+    index = np.random.choice(len(X_test)) # or X_test.shape[0]
+    image = X_test[index, :, :] # gives random row
+    image_arr = np.reshape(image, (1, 784))
+
+    return model_all_layers.predict(image_arr)
 
 
-
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        predictions, image = get_prediction()
+        final_preds = [pred.tolist() for pred in predictions]
+        return json.dumps({
+            'prediction': final_preds,
+            'image': image.tolist()
+        })
     return 'Welcome to the model server!'
 
 if __name__ == '__main__':
---
+++ mnist_neural_network/streamlit.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import numpy as np
+import json
+import requests
+import matplotlib.pyplot as plt
+
+URI = 'http://127.0.0.1:5000/' #currently my model server
